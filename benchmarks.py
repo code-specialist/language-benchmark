15233c0a7aa991448775ed502e9c218c257dfba9
Jonas Scholl <jonas@scholl.tech>
2022-12-19 13:07:03 +0100

    refactoring & improve cmd output

--- benchmarks.py
+++ benchmarks.py
@@ -1,50 +1,176 @@
-import json
 import statistics
+import sys
 from collections import defaultdict
+from dataclasses import dataclass
+from functools import lru_cache
+from io import BytesIO
+from math import ceil, floor
 from time import perf_counter
-from typing import Dict, List, Type
+from typing import Dict, List, Type, Self
+
+from PIL import Image
+from matplotlib import pyplot as plt
 
 from processors.abstract import BenchmarkProcessor
 
 
+@dataclass
+class BenchmarkConfiguration:
+    processors: list[Type[BenchmarkProcessor]]
+
+    # benchmarks
+    runs: int = 30
+    iterations_per_run: int = 10
+    input_length_min: int = 100
+    input_length_max: int = 10000
+
+    # visualization
+    background_color: str = "#111827"
+    dpi: int = 150
+
+
 class Benchmarks:
 
-    def __init__(self, processors: List[Type[BenchmarkProcessor]], runs: int, tries_per_run: int = 30, list_length_min: int = 100, list_length_max: int = 100000):
-        self.list_length_min = list_length_min
-        self.list_length_max = list_length_max
-        self.runs = runs
-        self.tries_per_run = tries_per_run
-        self.results: Dict[Type[BenchmarkProcessor], List[float]] = defaultdict(lambda: [])
-        self.processors = processors
+    def __init__(self, configuration: BenchmarkConfiguration):
+        self.configuration = configuration
+        self._results: dict[Type[BenchmarkProcessor], list[float]] = defaultdict(lambda: [])
+        self._frames = []
 
-    def run(self):
+    def run(self) -> Self:
         print("\nPreparing Benchmarks...")
-        for processor in self.processors:
+        for processor in self.configuration.processors:
             processor.prepare()
-        print("Benchmarks prepared, let's go!")
+        print("Benchmarks prepared, let's go!\n")
 
-        print(f"\nStarting Benchmarks - {self.runs} runs")
+        run_counter, step_width = 0, ceil((self.configuration.input_length_max - self.configuration.input_length_min) / self.configuration.runs)
+        for list_length in range(self.configuration.input_length_min, self.configuration.input_length_max, step_width):
+            if run_counter > self.configuration.runs:
+                break
 
-        run_counter = 0
-        for list_length in range(self.list_length_min, self.list_length_max, int((self.list_length_max - self.list_length_min) / self.runs)):
             run_counter += 1
-            print(f"\n\t Run {run_counter}")
-            for processor in self.processors:
 
+            for processor in self.configuration.processors:
+                self._display_benchmark_progress(run=run_counter, current_language=processor.language())
                 tries = []
-                for _ in range(self.tries_per_run):
+                for _ in range(self.configuration.iterations_per_run):
                     start = perf_counter()
                     processor.process(list_length=list_length)
                     end = perf_counter()
                     tries.append(end - start)
 
                 average_processing_time = statistics.mean(tries)
-                print(f"\t\t[{processor.language()}] - {average_processing_time} seconds")
-
-                self.results[processor].append(average_processing_time)
+                self._results[processor].append(average_processing_time)
 
         print("\nBenchmarks finished!\n")
 
-    def export(self, filename: str):
-        with open(filename, "w") as file:
-            json.dump(self.results, file)
+        return self
+
+    def visualize(self) -> Self:
+        if not self._results:
+            raise ValueError("Could not visualize Benchmarks, they didn't run yet")
+
+        plots: Dict[Type[BenchmarkProcessor], List[float]] = {}
+        total_width = len(tuple(self._results.values())[0])
+        total_frames = total_width * len(self._results.keys()) * 2
+
+        def visualize_step():
+            bytes_object = BytesIO()
+
+            plt.figure(facecolor=self.configuration.background_color, dpi=self.configuration.dpi)
+            plt.xlim(0, total_width)
+            plt.ylim(0, self._get_longest_run() * 1.1)
+
+            for processor, points in plots.items():
+                points_to_plot = [*points, *([None for _ in range(total_width - len(points))])]
+                plt.plot(range(total_width), points_to_plot, processor.color(), label=processor.language())
+
+            plt.tight_layout()
+            plt.axis("off")
+            plt.legend(loc="upper left", facecolor=self.configuration.background_color, labelcolor="white", frameon=False)
+            plt.savefig(bytes_object)
+            plt.close("all")
+
+            self._frames.append(Image.open(bytes_object))
+            self._display_visualization_progress(current_frame=len(self._frames), total_frames=total_frames)
+
+        for language, benchmark_times in self._results.items():
+            for index in range(len(benchmark_times)):
+                plots[language] = benchmark_times[:index + 1]
+                visualize_step()
+
+        for _ in range(int(len(self._frames))):
+            visualize_step()
+
+        print("\nData Visualization finished!\n")
+
+        return self
+
+    def create_gif(self) -> Self:
+        if not self._frames:
+            raise ValueError("Could not create GIF - Benchmarks aren't visualized")
+
+        print("Creating GIF...")
+
+        frame_one = self._frames[0]
+        frame_one.save(
+            "benchmark.gif",
+            format="GIF",
+            append_images=self._frames,
+            save_all=True,
+            duration=30,
+            loop=0,
+        )
+
+        print("Successfully created GIF!")
+
+        return self
+
+    @property
+    def results(self) -> dict[Type[BenchmarkProcessor], list[float]]:
+        return self._results
+
+    def _display_benchmark_progress(self, run: int, current_language: str, bar_length=50) -> None:
+        progress = run / self.configuration.runs
+        bar = self._progress_bar_str(progress=progress, width=bar_length)
+        language = self._current_language_str(current_language=current_language)
+        run_progress = self._benchmark_run_progress_str(run=run)
+        progress_percent = round(progress * 100, 1)
+        sys.stdout.write(f'Running Benchmarks | Run {run_progress} {language} - {bar} {progress_percent}%\r')
+        sys.stdout.flush()
+
+    def _display_visualization_progress(self, current_frame: int, total_frames: int, bar_length=50) -> None:
+        progress = current_frame / total_frames
+        bar = self._progress_bar_str(progress=progress, width=bar_length)
+        progress_percent = round(progress * 100, 1)
+        sys.stdout.write(f'Visualizing Benchmarks - {bar} {progress_percent}%\r')
+        sys.stdout.flush()
+
+    @staticmethod
+    def _progress_bar_str(progress: float, width: int) -> str:
+        progress = min(1., max(0., progress))
+        whole_width = floor(progress * width)
+        remainder_width = (progress * width) % 1
+        part_width = floor(remainder_width * 8)
+        part_char = [" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉"][part_width]
+        if (width - whole_width - 1) < 0:
+            part_char = ""
+        line = "█" * whole_width + part_char + " " * (width - whole_width - 1)
+        return line
+
+    def _benchmark_run_progress_str(self, run: int) -> str:
+        max_run_digits = len(str(self.configuration.runs))
+        current_run_digits = len(str(run))
+        return f'{"0" * (max_run_digits - current_run_digits)}{run}/{self.configuration.runs}'
+
+    @lru_cache()
+    def _current_language_str(self, current_language: str) -> str:
+        languages = [processor.language() for processor in self.configuration.processors]
+        max_language_character_size = max(map(len, languages))
+        current_language_character_size = len(current_language)
+        return f'[{current_language}]{" " * (max_language_character_size - current_language_character_size)}'
+
+    def _get_longest_run(self) -> float:
+        longest_run = 0
+        for benchmarks in self._results.values():
+            longest_run = max(longest_run, max(benchmarks))
+        return longest_run
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from benchmarks import Benchmarks
+from benchmarks import Benchmarks, BenchmarkConfiguration
 from processors import PythonBenchmarkProcessor, JavaBenchmarkProcessor, JavaScriptBenchmarkProcessor, CPPBenchmarkProcessor
-from visualize import BenchmarkVisualizer
 
 if __name__ == '__main__':
-    processors = [
-        PythonBenchmarkProcessor,
-        JavaBenchmarkProcessor,
-        JavaScriptBenchmarkProcessor,
-        CPPBenchmarkProcessor,
-    ]
+    configuration = BenchmarkConfiguration(
+        processors=[
+            PythonBenchmarkProcessor,
+            JavaBenchmarkProcessor,
+            JavaScriptBenchmarkProcessor,
+            CPPBenchmarkProcessor,
+        ],
 
-    benchmarks = Benchmarks(processors=processors, runs=30, tries_per_run=3, list_length_max=1000)
-    benchmarks.run()
+        # benchmarks
+        iterations_per_run=1,
+        input_length_max=1000,
 
-    visualizer = BenchmarkVisualizer(benchmarks=benchmarks.results)
-    visualizer.visualize()
-    visualizer.create_gif()
+        # visualization
+        dpi=50,
+    )
+
+    benchmarks = Benchmarks(configuration) \
+        .run() \
+        .visualize() \
+        .create_gif()
